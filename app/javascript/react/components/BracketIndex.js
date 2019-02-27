8f039eb3bd7cae67a7293dcce23692071b6d9a90
Marc Marino <marc.a.marino@gmail.com>
2019-02-27 16:22:16 -0500

    able to delete a bracket

--- app/javascript/react/components/BracketIndex.js
+++ app/javascript/react/components/BracketIndex.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import BracketTile from './BracketTile';
+import swal from 'sweetalert';
 
 class BracketIndex extends React.Component {
   constructor(props) {
     super(props)
       this.state = {
-        brackets: []
+        brackets: [],
+        error: ''
       }
+    this.onDelete = this.onDelete.bind(this)
+  }
+
+  deleteBracket(id) {
+    return fetch(`/api/v1/users/${window.currentUser.id}/brackets/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'},
+      credentials: 'same-origin'
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`;
+            error = new Error(errorMessage);
+          throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      if (body.error) {
+        throw body.error
+      }
+      swal(body.title, body.text, body.type)
+      this.setState({
+        brackets: this.state.brackets.filter(bracket => bracket.id !== body.bracket_id)
+      });
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
+  onDelete(event) {
+    event.preventDefault();
+
+    let id = event.target.id
+
+    swal({
+      title: "Delete Bracket",
+      text: "Once deleted, this bracket is gone, gone, gone away",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        this.deleteBracket(id)
+      } else {
+        swal("FINE....", "Don't delete the bracket")
+      }
+    })
   }
 
   componentDidMount() {
@@ -29,7 +83,6 @@ class BracketIndex extends React.Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
 
-
   render() {
     let brackets = this.state.brackets.map(bracket => {
       return(
@@ -37,6 +90,7 @@ class BracketIndex extends React.Component {
           key={bracket.id}
           id={bracket.id}
           name={bracket.name}
+          deleteBracket={this.onDelete}
         />
       )
     })
--- app/javascript/react/components/BracketTile.js
+++ app/javascript/react/components/BracketTile.js
@@ -3,7 +3,19 @@ import React from 'react';
 const BracketTile = props => {
   return(
     <div>
-      <li>{props.name}</li>
+      <li className='listItemMargins'>
+        {props.name}
+        <a className='editLink listItemMargins'>
+          <i className="fas fa-edit"></i>
+        </a>
+        <a className='deleteLink listItemMargins'>
+          <i className="far fa-times-circle"
+             id={props.id}
+             onClick={props.deleteBracket}
+          >
+          </i>
+        </a>
+      </li>
     </div>
   )
 }
